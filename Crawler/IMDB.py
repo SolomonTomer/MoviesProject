297bc513fcc39d242d8a495fb8828446238ca15d
SolomonTomer <solo.tomer@gmail.com>
2017-08-08 22:06:56 +0300

    Saving changes aside

--- Crawler/IMDB.py
+++ Crawler/IMDB.py
@@ -7,21 +7,26 @@ SEARCH_URL = "http://www.imdb.com/find?ref_=nv_sr_fn&q={}&s=all"
 log = open('logger.txt', 'w')
 
 
-def search_and_paras(movies):
+def parse_movie_page(page):
+
+def search_and_parse(movies):
     for movie in movies:
-        query = movie
-        req = Request(MOVIES_LIST, headers={'User-Agent': 'Mozilla/5.0'})
-    main_page = BeautifulSoup(urlopen(req).read(), "html.parser")
-    links = main_page.find_all("div", "title_bump")[0].find_all("a", "link_tab", href=True)
-    for i in range(STARTING_INDEX, len(links)):
-        extract_movies_list(BASIC_URL + links[i]['href'], 0, movies_list)
-    return movies_list
+        query = SEARCH_URL.format(movie.title)
+        req = Request(query, headers={'User-Agent': 'Mozilla/5.0'})
+        main = BeautifulSoup(urlopen(req).read(), "html.parser")
+        for section in main.find_all("div", "findSection"):
+            if section.h3 is not None and section.h3.text == 'Title':
+                movie_link = section.find("td", "result_text").find("a", href=True)['href']
 
 
 # Main
-log.write("Starting process\n")
-lst = parse_metascore()
-log.write("Starting insertion\n")
-mv = Movie()
-mv.insert_to_temp(lst)
-log.write("DONE")
+query = SEARCH_URL.format("Spider-Man")
+req = Request(query, headers={'User-Agent': 'Mozilla/5.0'})
+main = BeautifulSoup(urlopen(req).read(), "html.parser")
+for section in main.find_all("div", "findSection"):
+    if section.h3 is not None and section.h3.text == 'Title':
+        movie_link = section.find("td", "result_text").find("a", href=True)['href']
+
+
+print(main)
+
