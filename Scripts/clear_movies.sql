372f2cc18a06a9094aeaf14307050af63f3283db
Tomer Solomon <helo.to@gmail.com>
2017-08-13 20:24:04 +0300

    Adding creation scripts

---
+++ Scripts/clear_movies.sql
@@ -0,0 +1,7 @@
+delete from movie_countries;
+delete from movie_directors;
+delete from movies;
+delete from movie_genres;
+delete from movie_stars;
+delete from movie_languages;
+delete from movie_writers;
