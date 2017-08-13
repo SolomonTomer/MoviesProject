ccc6edb25f425d733e64c4c29e77402edbc997e7
Tomer Solomon <helo.to@gmail.com>
2017-08-13 20:18:38 +0300

    Adding IMDB parser. Extracting data and creating a new DB.

--- Crawler/Imdb.py
+++ Crawler/Imdb.py
@@ -1,32 +1,129 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
+from urllib import parse
+from Crawler.Movie import MetaCriticMovie
 from Crawler.Movie import Movie
-import re
+import sys
 
-SEARCH_URL = "http://www.imdb.com/find?ref_=nv_sr_fn&q={}&s=all"
+#SEARCH_URL = "http://www.imdb.com/find?ref_=nv_sr_fn&q={}&s=all"
+SEARCH_URL = "http://www.imdb.com/find?ref_=nv_sr_fn&%s=all"
+BASE_URL = "http://www.imdb.com"
 log = open('logger.txt', 'w')
 
 
-def parse_movie_page(page):
+def parse_movie_page(link, cur_movie):
+    try:
+        p_req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
+        page = BeautifulSoup(urlopen(p_req).read(), "html.parser")
+        rate_type = page.find("div","title_wrapper").find(itemprop="contentRating")
+        if rate_type is not None:
+            cur_movie.rated = rate_type.get("content")
+        imdb_rating = page.find("div","ratingValue")
+        if imdb_rating is not None:
+            cur_movie.imdb_score = imdb_rating.span.text
 
-def search_and_parse(movies):
+        # Credit summary data
+        credits_section = page.find("div", "plot_summary")
+        if credits_section is not None:
+            # Handling director & writers
+            directors = []
+            writers = []
+            for director in credits_section.find_all(itemprop="director"):
+                directors.append(director.span.text)
+            for writer in credits_section.find_all(itemprop="creator"):
+                writers.append(writer.span.text)
+            cur_movie.directors = directors
+            cur_movie.writers = writers
+
+        # Main actors
+        actors = []
+        actors_table = page.find(id="titleCast")
+        if actors_table is not None:
+            actors_table = actors_table.table.find_all("tr")
+            for actor_index in range(1, len(actors_table)):
+                if actor_index > 5:
+                    break
+                tname = actors_table[actor_index].find("span", "itemprop").text
+                actors.append(tname)
+        cur_movie.stars = actors
+
+        # Box office data
+        box_office_section = page.find("h3", text="Box Office")
+        ## TODO:: Check if can remove the $ sign from all data
+
+        if box_office_section is not None:
+
+            # Some blocks can be missed
+            budget = 0
+            gross = 0
+            opening_weekend = 0
+            div_elem = box_office_section.findNext("div")
+            while True:
+                if div_elem.h4.text[:-1] is not None and div_elem.h4.text[:-1] == "Budget":
+                    budget = div_elem.h4.nextSibling.strip()[1:].replace(',', '')
+                elif div_elem.h4.text[:-1] is not None and div_elem.h4.text[:-1] == "Gross":
+                    gross = div_elem.h4.nextSibling.strip()[1:].replace(',', '')
+                elif div_elem.h4.text[:-1] is not None and div_elem.h4.text[:-1] == "Opening Weekend":
+                    num_ending = div_elem.h4.nextSibling.strip().index(" ")
+                    opening_weekend = div_elem.h4.nextSibling.strip()[1:num_ending].replace(',', '')
+                else:
+                    break
+                div_elem = div_elem.findNext("div")
+            cur_movie.budget = budget
+            cur_movie.opening_weekend = opening_weekend
+            cur_movie.gross = gross
+
+        languages = []
+        for cur_lang in page.find(text="Language:").parent.parent.find_all("a"):
+            languages.append(cur_lang.text)
+        cur_movie.languages = languages
+        counties = []
+        for cur_country in page.find(text="Country:").parent.parent.find_all("a"):
+            counties.append(cur_country.text)
+        cur_movie.countries = counties
+    except:
+        print(sys.exc_info()[0])
+        log.write("Link:" + link + ". \n" + sys.exc_info()[0] + "\n\n")
+        raise
+
+
+def search_and_parse(movies, movie_list):
     for movie in movies:
-        query = SEARCH_URL.format(movie.title)
-        req = Request(query, headers={'User-Agent': 'Mozilla/5.0'})
-        main = BeautifulSoup(urlopen(req).read(), "html.parser")
-        for section in main.find_all("div", "findSection"):
-            if section.h3 is not None and section.h3.text == 'Title':
-                movie_link = section.find("td", "result_text").find("a", href=True)['href']
+        #query = SEARCH_URL.format("Alléluia")
+        q = parse.urlencode({"q": movie.get("title")})
+        query = SEARCH_URL % q
+        #print("Alléluia" + " " + query + "\n")
+        try:
+            req = Request(query, headers={'User-Agent': 'Mozilla/5.0'})
+            main = BeautifulSoup(urlopen(req).read(), "html.parser")
+            for section in main.find_all("div", "findSection"):
+                if section.h3 is not None and section.h3.text == 'Titles':
+                    try:
+                        movie_link = section.find("td", "result_text").find("a", href=True)['href']
+                        full_movie = Movie()
+                        full_movie.copy_from_meta(movie)
+                        parse_movie_page(BASE_URL + movie_link, full_movie)
+                        movie_list.append(full_movie)
+                        break
+                    except:
+                        log.write("Error link: " + movie_link + "\n")
+                        break
+        except:
+            log.write("Error opening a url.\n")
+            continue
 
 
 # Main
-query = SEARCH_URL.format("Spider-Man")
-req = Request(query, headers={'User-Agent': 'Mozilla/5.0'})
-main = BeautifulSoup(urlopen(req).read(), "html.parser")
-for section in main.find_all("div", "findSection"):
-    if section.h3 is not None and section.h3.text == 'Title':
-        movie_link = section.find("td", "result_text").find("a", href=True)['href']
-
-
-print(main)
-
+try:
+    lst = []
+    log.write("starting\n")
+    rows = MetaCriticMovie.get_all_rows()
+    #rows = MetaCriticMovie.get_by_title("Alléluia")
+    #rows = ['f', 'f']
+    search_and_parse(rows, lst)
+    Movie.create_insert(lst)
+except:
+    raise
+finally:
+    log.write("ending\n")
+    log.close()
--- Crawler/Movie.py
+++ Crawler/Movie.py
@@ -1,10 +1,11 @@
-import pymysql
+import sys
 from Crawler.Database import Database
 
 
 class MetaCriticMovie:
     raw_sql = 'select "{}", STR_TO_DATE("{}", "%d/%m/%Y"), "{}", "{}", "{}", "{}" from dual'
     get_all_sql = 'select * from movie_tmp'
+    get_movie_by_title = "select * from movie_tmp where title= %s"
 
     def __init__(self, title, release_date, run_time, meta_score, user_score, genres):
         self.title = title
@@ -15,6 +16,18 @@ class MetaCriticMovie:
         self.genres = genres
 
     @staticmethod
+    def get_by_title(title):
+        try:
+            conn = Database().get_connection()
+            cursor = conn.cursor()
+            cursor.execute(MetaCriticMovie.get_movie_by_title, title)
+            results = cursor.fetchall()
+            return results
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            raise
+
+    @staticmethod
     def create_insert(lst):
         union = ''
         for i in range(len(lst)):
@@ -23,53 +36,144 @@ class MetaCriticMovie:
                                                     self.meta_score, self.user_score, ', '.join(self.genres))
             if i + 1 != len(lst):
                 union += " union all \n"
-            # m = lst[i]
-            # m.replace('\'', '\\\'')
-            # union += 'select \'' + m[0] + '\',' + m[1] + ', STR_TO_DATE(\'' + m[2] + '\', \'%d/%m/%Y\') from dual'
-            # if i + 1 != len(lst):
-            #     union += ' union all \n'
         print(union)
 
     @staticmethod
     def get_all_rows():
-
+        results = ''
+        try:
+            conn = Database().get_connection()
+            cursor = conn.cursor()
+            cursor.execute(MetaCriticMovie.get_all_sql)
+            results = cursor.fetchall()
+            return results
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+            raise
 
 
 class Movie:
 
-    def __init__(self, row):
+    def __init__(self):
+        self.title = None
+        self.release_date = None
+        self.rated = None
+        self.run_time = 0
+        self.country = []
+        self.language = []
+        self.budget = 0
+        self.opening_weekend = 0
+        self.gross = 0
+        self.imdb_score = 0
+        self.meta_score = 0
+        self.user_score = 0
+        self.genres = []
+        self.director = []
+        self.writer = []
+        self.stars = []
+
+    def copy_from_row(self, row):
         self.title = row[0]
         self.release_date = row[2]
         self.rated = None
         self.run_time = 0
-        self.country = None
-        self.language = None
+        self.country = []
+        self.language = []
+        self.budget = 0
         self.opening_weekend = 0
         self.gross = 0
         self.imdb_score = 0
         self.meta_score = row[1]
         self.user_score = 0
         self.genres = []
-        self.director = None
-        self.writer = None
+        self.director = []
+        self.writer = []
         self.stars = []
 
-    def get_temp_movies(self):
-        query = 'Select * from movie_tmp'
-        conn = Database().get_connection()
-        cursor = conn.cursor()
-        cursor.execute(query)
-        results = cursor.fetchall()
+    def copy_from_meta(self, meta_movie):
+        self.title = meta_movie.get("title")
+        self.release_date = meta_movie.get("release_date")
+        self.rated = None
+        self.run_time = meta_movie.get("run_time")
+        self.budget = 0
+        self.opening_weekend = 0
+        self.gross = 0
+        self.imdb_score = 0
+        self.meta_score = meta_movie.get("meta_score")
+        self.user_score = meta_movie.get("user_score")
+        self.genres = meta_movie.get("genres").split(", ")
+        self.countries = []
+        self.languages = []
+        self.directors = []
+        self.writers = []
+        self.stars = []
 
+    movies_union_sql = 'select "{}", STR_TO_DATE("{}", "%Y-%m-%d"), "{}", "{}", "{}", "{}", "{}", "{}", "{}", "{}" ' \
+                       'from dual'
+    table_connector_union_sql = 'select "{}", "{}" from dual '
 
+    @staticmethod
+    def generate_union(lst, title, add_union):
+        res = ''
+        if add_union:
+            res += " union all \n"
+        for ind in range(len(lst)):
+            res += Movie.table_connector_union_sql.format(title, lst[ind])
+            if ind + 1 != len(lst):
+                res += " union all \n"
+        return res
 
+    @staticmethod
+    def create_insert(lst):
+        movies_insert = 'insert into movies \n'
+        genres_insert = 'insert into movie_genres \n'
+        countries_insert = 'insert into movie_countries \n'
+        languages_insert = 'insert into movie_languages \n'
+        directors_insert = 'insert into movie_directors \n'
+        writers_insert = 'insert into movie_writers \n'
+        stars_insert = 'insert into movie_stars \n'
+        for i in range(len(lst)):
+            self = lst[i]
+            movies_insert += Movie.movies_union_sql.format(self.title, self.release_date, self.rated, self.run_time, self.budget,
+                                                    self.opening_weekend, self.gross, self.imdb_score,
+                                                    self.meta_score, self.user_score)
+            genres_insert += Movie.generate_union(self.genres, self.title, False if i == 0 else True)
+            countries_insert += Movie.generate_union(self.countries, self.title, False if i == 0 else True)
+            languages_insert += Movie.generate_union(self.languages, self.title, False if i == 0 else True)
+            directors_insert += Movie.generate_union(self.directors, self.title, False if i == 0 else True)
+            writers_insert += Movie.generate_union(self.writers, self.title, False if i == 0 else True)
+            stars_insert += Movie.generate_union(self.stars, self.title, False if i == 0 else True)
+            #
+            # for g in self.genres:
+            #     genres_insert += Movie.table_connector_union_sql.format(self.title, g)
+            # for g in self.countries:
+            #     countries_insert += Movie.table_connector_union_sql.format(self.title, g)
+            # for g in self.languages:
+            #     languages_insert += Movie.table_connector_union_sql.format(self.title, g)
+            # for g in self.directors:
+            #     directors_insert += Movie.table_connector_union_sql.format(self.title, g)
+            # for g in self.writers:
+            #     writers_insert += Movie.table_connector_union_sql.format(self.title, g)
+            # for g in self.stars:
+            #     stars_insert += Movie.table_connector_union_sql.format(self.title, g)
 
-        # Received an error while trying to insert a valid statement. splitted manualy
-        # try:
-        #     cursor = conn.cursor()
-        #     sql = 'INSERT INTO movie_tmp ' + union
-        #     cursor.execute(sql)
-        #     conn.commit()
-        #     result = cursor.fetchone()
-        # finally:
-        #     conn.close()
+            if i + 1 != len(lst):
+                movies_insert += " union all \n"
+
+        # Removing "None"
+        movies_insert = movies_insert.replace('"None"', 'NULL')
+
+        print("#MOVIES\n")
+        print(movies_insert + ";")
+        print("#GENRES\n")
+        print(genres_insert+ ";")
+        print("#COUNTRIES\n")
+        print(countries_insert + ";")
+        print("#LANGUAGES\n")
+        print(languages_insert + ";")
+        print("#DIRECTORS\n")
+        print(directors_insert + ";")
+        print("#WRITERS\n")
+        print(writers_insert + ";")
+        print("#STARS\n")
+        print(stars_insert + ";")
